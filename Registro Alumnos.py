e701de6a58ccf441dc330a907f6a08dcb5bb1fa3
Pau <madrigalpaula0@gmail.com>
2025-03-21 15:58:07 -0600

    Parte Pau

---
+++ Registro Alumnos.py
@@ -0,0 +1,15 @@
+alumA = set()
+alumB = set()
+
+while True:
+    print("\nSistema de Registro de Alumnos y Cursos")
+    print("1. Agregar alumno al Curso A")
+    print("2. Agregar alumno al Curso B")
+    print("3. Lista de alumnos en ambos cursos ")
+    print("4. Lista de alumnos inscritos en los dos cursos ")
+    print("5. Lista de alumnos solo en el Curso A ")
+    print("6. Lista de alumnos solo en el Curso B ")
+    print("7. Lista de alumnos inscritos en un solo curso pero no en ambos ")
+    print("8. Salir")
+
+    opcion = input("Seleccione una opción: ")
